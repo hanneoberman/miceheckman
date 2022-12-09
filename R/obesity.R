07f231c3772d81a4546ed97c83829b51deee81bf
hanneoberman <h.i.oberman@uu.nl>
2022-12-09 17:33:46 +0100

    Add imputation function, example data and github actions

---
+++ R/obesity.R
@@ -0,0 +1,18 @@
+#' Incomplete clustered data related to obesity
+#'
+#' Data adapted from https://www.kaggle.com/datasets/fabinmndez/obesitydata.
+#'
+#' @format ## `obesity`
+#' A data frame with 2,111 rows and 7 columns:
+#' \describe{
+#'   \item{cluster}{Grouping variable}
+#'   \item{gender}{Gender (0 = male, 1 = female)}
+#'   \item{age}{Age (in years)}
+#'   \item{height}{Height (in meters)}
+#'   \item{weight}{Weight (in kilograms)}
+#'   \item{bmi}{Body Mass Index (in kilograms per square meter)}
+#'   \item{rt}{Response time (in minutes)}
+#'   ...
+#' }
+#' @source <https://www.kaggle.com/datasets/fabinmndez/obesitydata>
+"obesity"
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# miceheckman
+<!-- badges: start -->
+[![R-CMD-check](https://github.com/hanneoberman/miceheckman/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/hanneoberman/miceheckman/actions/workflows/R-CMD-check.yaml)
+<!-- badges: end -->
+
+# The `R` package `miceheckman`
+
 Additional imputation function for the `R` package `mice`, according to the Heckman model.
